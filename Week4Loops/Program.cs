b0342a18f7feee0e1a7e203d34991d24abf28a0f
yavuz06 <yavuzselmkaya@gmail.com>
2023-03-24 18:23:26 +0300

    week4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+Random rnd = new Random();
+int myrandom;
+
+for (int i = 0; i<3 ; i++)
+{
+    myrandom = rnd.Next(1, 11);
+    Console.WriteLine(myrandom);
+}
+
+//for (int i = 0; i < 5; i++) 
+//{
+//    Console.WriteLine($"{i +1}. i will not skip class again");
+//}
---
+++ Week4WhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Random rnd = new Random();
+int cpurandom = rnd.Next(1, 11);
+
+int nooftries = 0;
+
+
+while (nooftries < 3)
+{
+    Console.WriteLine("try to guess the cpu number:");
+    int userguess = Convert.ToInt32(Console.ReadLine());
+    nooftries++;
+    
+    if(userguess == cpurandom) 
+    {
+        Console.WriteLine("you won");
+        break;
+    }
+    
+}
+Console.WriteLine($"number of tries:{nooftries}");
+Console.WriteLine("have a nice day");
